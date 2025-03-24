3e6d295e318947c5ff05d324c2a1df5918877050
JaceTong <167060051+JaceTong@users.noreply.github.com>
2025-03-25 10:28:37 +1300

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,1 +1,6 @@
 import Add_Decrese_Value
+
+
+menubar=nuke.menu("Nodes")
+menubar.addMenu("Add_Decrese").addCommand('Add_Value','Add_Decrese_Value.Add_value()', "e", shortcutContext=2)
+menubar.addMenu("Add_Decrese").addCommand('Decrese_value','Add_Decrese_Value.De_value()', "shift+e", shortcutContext=2)
